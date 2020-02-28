7bb8b75d1de819ed4dc278a2a68f59c9616b11fa
Dmytro Serdiukov <dmytro.serdiukov@gmail.com>
2020-02-28 12:44:51 +0200

    add profile status, login form

--- src/common/Loader/LoginComponent.jsx
+++ src/common/Loader/LoginComponent.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
-const LoginComponent = () => {
-  return <div>
-    <h1>LOGIN FORM</h1>
-  </div>
+const LoginComponent = props => {
+  return (
+    <div>
+      <h2>LOGIN COMPONENT</h2>
+      <LoginForm />
+    </div>
+  );
+};
+
+const LoginForm = () => {
+  return <>
+    <form action="">
+        <div>
+            <input type="text" placeholder="Login"/>
+        </div>
+        <div>
+            <input type="text" placeholder="Password"/>
+        </div>
+        <div>
+            <input type="checkbox"/> remember me
+        </div>
+        <div>
+            <button>Submit</button>
+        </div>
+    </form>
+  </>
 }
 
 export default LoginComponent;
---
+++ src/components/Profile/ProfileStatus/ProfileStatus.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default class ProfileStatus extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+          
+        }
+    }
+
+  render() {
+    return <div>
+        <div>
+            <textarea name="" id="" cols="30" rows="10" placeholder="type something"></textarea>
+        </div>
+        <div>
+            <button></button>
+        </div>
+    </div>
+  }
+}
